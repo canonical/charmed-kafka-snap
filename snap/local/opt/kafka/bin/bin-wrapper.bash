2b5e2bf702dd277432428a12530c71f25f083fcc
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2024-03-27 21:59:01 +0000

    chore: add modified cc log4j defaults

--- snap/local/opt/kafka/bin/bin-wrapper.bash
+++ snap/local/opt/kafka/bin/bin-wrapper.bash
@@ -3,9 +3,7 @@
 set -e
 
 unset KAFKA_JMX_OPTS
+unset KAFKA_LOG4J_OPTS
 
-if [ "x$KAFKA_LOG4J_OPTS" = "x" ]; then
-    export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:${SNAP_DATA}/etc/kafka/tools-log4j.properties"
-fi
 
 ${SNAP}/opt/kafka/bin/${bin_script} "${@}"
