7c70507141ca42eac38fbe662eb5ed01ba520b02
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2024-05-28 00:07:43 +0100

    chore: add licence, teak logging

---
+++ snap/local/opt/cruise-control/bin/start-wrapper.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+if [ "x$KAFKA_LOG4J_OPTS" = "x" ]; then
+    export KAFKA_LOG4J_OPTS="-Dcruisecontrol.logs.dir=${LOG_DIR} -Dlog4j.configurationFile=${SNAP_DATA}/etc/cruise-control/log4j.properties"
+fi
+
+"${SNAP}"/usr/bin/setpriv \
+    --clear-groups \
+    --reuid snap_daemon \
+    --regid snap_daemon -- \
+    "${SNAP}/opt/cruise-control/bin/kafka-cruise-control-start.sh" "${SNAP_DATA}"/etc/cruise-control/cruisecontrol.properties
+
