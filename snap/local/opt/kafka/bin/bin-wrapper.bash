396435c904190e045880dfdb2281cd8c4cb9480e
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2024-05-29 12:46:18 +0100

    chore: use KAFKA_CFG_LOGLEVEL

--- snap/local/opt/kafka/bin/bin-wrapper.bash
+++ snap/local/opt/kafka/bin/bin-wrapper.bash
@@ -4,4 +4,8 @@ set -e
 
 unset KAFKA_JMX_OPTS
 
+if [ "x$KAFKA_LOG4J_OPTS" = "x" ]; then
+    export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:${SNAP_DATA}/etc/kafka/tools-log4j.properties -Dcharmed.kafka.log.level=WARN",
+fi
+
 ${SNAP}/opt/kafka/bin/${bin_script} "${@}"
--- snap/local/opt/kafka/bin/start-wrapper.bash
+++ snap/local/opt/kafka/bin/start-wrapper.bash
@@ -3,7 +3,7 @@
 set -e
 
 if [ "x$KAFKA_LOG4J_OPTS" = "x" ]; then
-    export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:${SNAP_DATA}/etc/kafka/log4j.properties"
+    export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:${SNAP_DATA}/etc/kafka/log4j.properties -Dcharmed.kafka.log.level=${KAFKA_CFG_LOGLEVEL:-INFO}",
 fi
 
 "${SNAP}"/usr/bin/setpriv \
