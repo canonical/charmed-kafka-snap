56e59b1524c07c1dc7f1ccb3bd84c60422d5ad54
Raúl Zamora Martínez <76525382+zmraul@users.noreply.github.com>
2024-07-18 17:03:20 +0200

    add jmx for cruise control (#44)

--- snap/local/opt/cruise-control/bin/start-wrapper.bash
+++ snap/local/opt/cruise-control/bin/start-wrapper.bash
@@ -2,7 +2,13 @@
 
 set -e
 
-unset KAFKA_JMX_OPTS
+# Cruise control uses the same var for JMX metrics. To allow both to be used on the same machine, 
+# use a specific var for CC and override during startup.
+if [ "x$CC_JMX_OPTS" != "x" ]; then
+    export KAFKA_JMX_OPTS=${CC_JMX_OPTS}
+else
+    unset KAFKA_JMX_OPTS
+fi
 
 if [ "x$KAFKA_LOG4J_OPTS" = "x" ]; then
     export KAFKA_LOG4J_OPTS="-Dcruisecontrol.logs.dir=${LOG_DIR} -Dlog4j.configurationFile=${SNAP_DATA}/etc/cruise-control/log4j.properties -Dcruisecontrol.log.level=${KAFKA_CFG_LOGLEVEL:-INFO}"
