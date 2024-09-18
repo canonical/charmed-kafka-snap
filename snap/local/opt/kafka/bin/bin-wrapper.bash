b30f8d3ed8779caf0b9282351b3df2721a68cd7b
Raul Zamora <raul.zamora@canonical.com>
2024-09-18 13:20:44 +0200

    execute bin commands as snap_daemon

--- snap/local/opt/kafka/bin/bin-wrapper.bash
+++ snap/local/opt/kafka/bin/bin-wrapper.bash
@@ -8,4 +8,8 @@ if [ "x$KAFKA_LOG4J_OPTS" = "x" ]; then
     export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:${SNAP_DATA}/etc/kafka/tools-log4j.properties -Dcharmed.kafka.log.level=WARN",
 fi
 
-${SNAP}/opt/kafka/bin/${bin_script} "${@}"
+"${SNAP}"/usr/bin/setpriv \
+    --clear-groups \
+    --reuid snap_daemon \
+    --regid snap_daemon -- \
+    "${SNAP}/opt/kafka/bin/${bin_script}" "${@}"
