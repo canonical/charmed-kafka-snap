1e76daf2a7bee72bf4c565c52a6ce57252b03a58
Iman Enami <iman.enami@canonical.com>
2025-01-14 18:29:36 +0400

    improve logging defaults

--- snap/local/opt/kafka/bin/connect-wrapper.bash
+++ snap/local/opt/kafka/bin/connect-wrapper.bash
@@ -5,7 +5,7 @@ set -e
 unset KAFKA_JMX_OPTS
 
 if [ "x$KAFKA_LOG4J_OPTS" = "x" ]; then
-    export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:${SNAP_DATA}/etc/kafka/tools-log4j.properties -Dcharmed.kafka.log.level=WARN",
+    export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:${SNAP_DATA}/etc/kafka/log4j.properties -Dcharmed.kafka.log.level=INFO",
 fi
 
 ${SNAP}/opt/kafka/bin/connect-distributed.sh "${SNAP_DATA}"/etc/kafka/connect-distributed.properties
