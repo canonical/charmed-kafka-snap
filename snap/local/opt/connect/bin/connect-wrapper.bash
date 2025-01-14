d734b38f1b5b0fef3472cdd98689c9db93b23dcf
Iman Enami <iman.enami@canonical.com>
2025-01-14 19:13:00 +0400

    refactor & add connect component

---
+++ snap/local/opt/connect/bin/connect-wrapper.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+unset KAFKA_JMX_OPTS
+
+if [ "x$KAFKA_LOG4J_OPTS" = "x" ]; then
+    export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:${SNAP_DATA}/etc/connect/log4j.properties -Dcharmed.kafka.log.level=INFO",
+fi
+
+${SNAP}/opt/kafka/bin/connect-distributed.sh "${SNAP_DATA}"/etc/connect/connect-distributed.properties
